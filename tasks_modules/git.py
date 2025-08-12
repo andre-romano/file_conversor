271100a9f8afb696df4a4f095a0cc862646ffd5f
Andre <Andre@domain.com>
2025-08-11 23:42:01 -0300

    ci: fix changelog generation

--- tasks_modules/git.py
+++ tasks_modules/git.py
@@ -10,32 +10,43 @@ from tasks_modules._config import *
 from tasks_modules import pypi, scoop
 
 
-@task
+@task(pre=[pypi.publish, scoop.publish,])
+def tag(c):
+    print(f"[bold] Creating tag {GIT_RELEASE} locally ... [/]")
+    c.run(f"git tag {GIT_RELEASE}")
+    print(f"[bold] Creating tag {GIT_RELEASE} locally ... OK [/]")
+
+
+@task(pre=[tag,])
 def changelog(c):
     """
     Generate CHANGELOG.md file
     """
     print(f"[bold] Generating CHANGELOG.md ... [/]")
+    changelog_path = Path("CHANGELOG.md")
+    if changelog_path.exists():
+        changelog_path.unlink()
+
     c.run(f"pdm run git-changelog")
-    if not Path("CHANGELOG.md").exists():
-        raise RuntimeError("CHANGELOG.md does not exist")
+    if not changelog_path.exists():
+        raise RuntimeError(f"{changelog_path} does not exist")
 
     result = c.run(f"git status", hide=True)
-    if all(f not in result.stdout for f in ["CHANGELOG.md", "pyproject.toml"]):
-        print(f"[bold] Skipping commit: no changes in CHANGELOG.md  [/]")
+    if all(f not in result.stdout for f in [str(changelog_path), "pyproject.toml"]):
+        print(f"[bold] Skipping commit: no changes in {changelog_path}  [/]")
         return
 
-    c.run(f"git add CHANGELOG.md pyproject.toml", hide=True)
-    c.run(f"git commit -m \"=> CHANGELOG.md for {PROJECT_VERSION}\"", hide=True)
+    c.run(f"git add {str(changelog_path)} pyproject.toml", hide=True)
+    c.run(f"git commit -m \"=> {changelog_path} for {PROJECT_VERSION}\"", hide=True)
     c.run(f"git push")
-    print(f"[bold] Generating CHANGELOG.md ... OK [/]")
+    print(f"[bold] Generating {changelog_path} ... OK [/]")
 
 
-@task(pre=[changelog, pypi.publish, scoop.publish,])
+@task(pre=[changelog,])
 def publish(c):
     """"Publish Git"""
     print(f"[bold] Publishing to GitHub ... [/]")
-    c.run(f"git tag {GIT_RELEASE}")
+    c.run(f"git push")
     c.run(f"git push --tags")
     print(f"[bold] Publishing to GitHub ... [/][bold green]OK[/]")
 
