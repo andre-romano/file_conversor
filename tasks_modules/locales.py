639824912fbf1eed4662765dca6bc0143d2f2d2e
Andre <Andre@domain.com>
2025-08-20 17:23:11 -0300

    feat: add language DE, EN, ES, FR, HI, PT

--- tasks_modules/locales.py
+++ tasks_modules/locales.py
@@ -10,6 +10,36 @@ from tasks_modules import _config
 from tasks_modules._config import *
 
 
+def _translate_locale(path: Path):
+    exception = None
+
+    locale = path.name
+    lang_id = locale.split("_")[0]
+
+    print(f"[bold] Translating locale '{locale}' ... [/]")
+    po = polib.pofile(path / "LC_MESSAGES" / "messages.po")
+
+    if not po.untranslated_entries():
+        print(f"[bold] Translating locale '{locale}' ... [/][bold yellow]NO CHANGES[/]")
+        return
+
+    translator = GoogleTranslator(source="en", target=lang_id)
+    for entry in po.untranslated_entries():
+        try:
+            print(f"\tTranslating '{lang_id}/{entry.msgid}' ... ", end="")
+            entry.msgstr = entry.msgid if locale == "en_US" else translator.translate(entry.msgid)
+            print(f"[bold green]OK[/]")
+        except Exception as e:
+            print(f"[bold red]FAILED[/]")
+            exception = e
+    po.save()
+
+    if exception:
+        print(f"[bold] Translating locale '{locale}' ... [/][bold red]FAILED[/]")
+        raise exception
+    print(f"[bold] Translating locale '{locale}' ... [/][bold green]OK[/]")
+
+
 @task
 def template(c: InvokeContext):
     """ Update locales template (.pot)"""
@@ -44,33 +74,19 @@ def update(c: InvokeContext):
 @task(pre=[update])
 def translate(c: InvokeContext):
     """ Translate locales' .PO files using Google Translate"""
+    exception = None
     for path in Path(I18N_PATH).glob("*"):
         if not path.is_dir():
             continue
-
-        locale = path.name
-        lang_id = locale.split("_")[0]
-        print(f"[bold] Translating locale '{locale}' ... [/]")
-        po = polib.pofile(path / "LC_MESSAGES" / "messages.po")
-
-        if not po.untranslated_entries():
-            print(f"[bold] Translating locale '{locale}' ... [/][bold yellow]NO CHANGES[/]")
-            continue
-
-        if locale == "en_US":
-            for entry in po.untranslated_entries():
-                entry.msgstr = entry.msgid
-        else:
-            translator = GoogleTranslator(source="en", target=lang_id)
-            for entry in po.untranslated_entries():
-                try:
-                    print(f"\tTranslating '{lang_id}/{entry.msgid}' ... ", end="")
-                    entry.msgstr = translator.translate(entry.msgid)
-                    print(f"[bold green]OK[/]")
-                except:
-                    print(f"[bold red]ERROR[/]")
-        po.save()
-        print(f"[bold] Translating locale '{locale}' ... [/][bold green]OK[/]")
+        try:
+            _translate_locale(path)
+        except Exception as e:
+            print(f"[bold red] ERROR:[/] {repr(e)}")
+            exception = e
+    if exception:
+        print(f"[bold yellow] WARN:[/] need to rerun [bold white]'locales.update'[/] - {repr(exception)}")
+        raise exception
+    print(f"[bold] Translation i18N:[/] [bold green]SUCCESS[/]")
 
 
 @task
