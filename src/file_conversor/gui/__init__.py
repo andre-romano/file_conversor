03e4ef66fcf4bb031f54810a4b84ddcbf941922e
Andre <Andre@domain.com>
2026-03-21 00:31:59 -0300

    code refactor, reduce code size and improve readability

--- src/file_conversor/gui/__init__.py
+++ src/file_conversor/gui/__init__.py
@@ -37,47 +37,6 @@ GUI_PATH = Environment.get_gui_folder()
 _ = get_translation()
 
 
-class MainSidebarFrame(SidebarFrame):
-    def __init__(self) -> None:
-        icon_size = (28, 28)  # px
-        btn_size = (45, 45)  # px
-
-        super().__init__(
-            gui_path=GUI_PATH,
-        )
-        self.btn_doc = ToolButton(icon=(ICON_PATH / "docx.ico", *icon_size), tooltip=_("Word tools"), btn_size=btn_size)
-        self.btn_xls = ToolButton(icon=(ICON_PATH / "xls.ico", *icon_size), tooltip=_("Excel tools"), btn_size=btn_size)
-        self.btn_ppt = ToolButton(icon=(ICON_PATH / "ppt.ico", *icon_size), tooltip=_("PowerPoint tools"), btn_size=btn_size)
-
-        self.btn_audio = ToolButton(icon=(ICON_PATH / "mp3.ico", *icon_size), tooltip=_("Audio tools"), btn_size=btn_size)
-        self.btn_video = ToolButton(icon=(ICON_PATH / "mp4.ico", *icon_size), tooltip=_("Video tools"), btn_size=btn_size)
-        self.btn_image = ToolButton(icon=(ICON_PATH / "jpg.ico", *icon_size), tooltip=_("Image tools"), btn_size=btn_size)
-
-        self.btn_pdf = ToolButton(icon=(ICON_PATH / "pdf.ico", *icon_size), tooltip=_("PDF tools"), btn_size=btn_size)
-        self.btn_ebook = ToolButton(icon=(ICON_PATH / "epub.ico", *icon_size), tooltip=_("Ebook tools"), btn_size=btn_size)
-        self.btn_text = ToolButton(icon=(ICON_PATH / "json.ico", *icon_size), tooltip=_("Text tools"), btn_size=btn_size)
-        self.btn_hash = ToolButton(icon=(ICON_PATH / "sha256.ico", *icon_size), tooltip=_("Hash tools"), btn_size=btn_size)
-
-        self.btn_info = ToolButton(icon=(ICON_PATH / "info.ico", *icon_size), tooltip=_("Info"), btn_size=btn_size)
-        self.btn_config = ToolButton(icon=(ICON_PATH / "repair.ico", *icon_size), tooltip=_("Settings"), btn_size=btn_size)
-
-        self.layout().addWidget(self.btn_doc)
-        self.layout().addWidget(self.btn_xls)
-        self.layout().addWidget(self.btn_ppt)
-        self.layout().addWidget(HLineFrame(shadow=HLineFrame.Shadow.Sunken))  # separator
-        self.layout().addWidget(self.btn_audio)
-        self.layout().addWidget(self.btn_video)
-        self.layout().addWidget(self.btn_image)
-        self.layout().addWidget(HLineFrame(shadow=HLineFrame.Shadow.Sunken))  # separator
-        self.layout().addWidget(self.btn_pdf)
-        self.layout().addWidget(self.btn_ebook)
-        self.layout().addWidget(self.btn_text)
-        self.layout().addWidget(self.btn_hash)
-        self.layout().addStretch()
-        self.layout().addWidget(self.btn_info)
-        self.layout().addWidget(self.btn_config)
-
-
 class MainWindowGUI(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -88,27 +47,49 @@ class MainWindowGUI(QMainWindow):
         self.setWindowIcon(QIcon(str(icon_path)))
         self.resize(800, 540)
 
-        # Sidebar, and Router
-        sidebar_widget = MainSidebarFrame()
-        stacked_widget = StackedRouter([
-            (DocFrame, sidebar_widget.btn_doc),
-            (XlsFrame, sidebar_widget.btn_xls),
-            (PptFrame, sidebar_widget.btn_ppt),
-            (AudioFrame, sidebar_widget.btn_audio),
-            (VideoFrame, sidebar_widget.btn_video),
-            (ImageFrame, sidebar_widget.btn_image),
-            (PdfFrame, sidebar_widget.btn_pdf),
-            (EbookFrame, sidebar_widget.btn_ebook),
-            (TextFrame, sidebar_widget.btn_text),
-            (HashFrame, sidebar_widget.btn_hash),
-            (InfoFrame, sidebar_widget.btn_info),
-            (ConfigFrame, sidebar_widget.btn_config),
+        # Sidebar
+        icon_size = (28, 28)  # px
+        btn_size = (45, 45)  # px
+
+        sidebar = SidebarFrame(gui_path=GUI_PATH)
+        sidebar.addItems(
+            btn_doc := ToolButton(icon=(ICON_PATH / "docx.ico", *icon_size), tooltip=_("Word tools"), btn_size=btn_size),
+            btn_xls := ToolButton(icon=(ICON_PATH / "xls.ico", *icon_size), tooltip=_("Excel tools"), btn_size=btn_size),
+            btn_ppt := ToolButton(icon=(ICON_PATH / "ppt.ico", *icon_size), tooltip=_("PowerPoint tools"), btn_size=btn_size),
+            HLineFrame(shadow=HLineFrame.Shadow.Sunken),  # separator
+            btn_audio := ToolButton(icon=(ICON_PATH / "mp3.ico", *icon_size), tooltip=_("Audio tools"), btn_size=btn_size),
+            btn_video := ToolButton(icon=(ICON_PATH / "mp4.ico", *icon_size), tooltip=_("Video tools"), btn_size=btn_size),
+            btn_image := ToolButton(icon=(ICON_PATH / "jpg.ico", *icon_size), tooltip=_("Image tools"), btn_size=btn_size),
+            HLineFrame(shadow=HLineFrame.Shadow.Sunken),  # separator
+            btn_pdf := ToolButton(icon=(ICON_PATH / "pdf.ico", *icon_size), tooltip=_("PDF tools"), btn_size=btn_size),
+            btn_ebook := ToolButton(icon=(ICON_PATH / "epub.ico", *icon_size), tooltip=_("Ebook tools"), btn_size=btn_size),
+            btn_text := ToolButton(icon=(ICON_PATH / "json.ico", *icon_size), tooltip=_("Text tools"), btn_size=btn_size),
+            btn_hash := ToolButton(icon=(ICON_PATH / "sha256.ico", *icon_size), tooltip=_("Hash tools"), btn_size=btn_size),
+            sidebar.getStretch(),
+            btn_info := ToolButton(icon=(ICON_PATH / "info.ico", *icon_size), tooltip=_("Info"), btn_size=btn_size),
+            btn_config := ToolButton(icon=(ICON_PATH / "repair.ico", *icon_size), tooltip=_("Settings"), btn_size=btn_size),
+        )
+
+        # Router
+        stacked_router = StackedRouter([
+            (DocFrame, btn_doc),
+            (XlsFrame, btn_xls),
+            (PptFrame, btn_ppt),
+            (AudioFrame, btn_audio),
+            (VideoFrame, btn_video),
+            (ImageFrame, btn_image),
+            (PdfFrame, btn_pdf),
+            (EbookFrame, btn_ebook),
+            (TextFrame, btn_text),
+            (HashFrame, btn_hash),
+            (InfoFrame, btn_info),
+            (ConfigFrame, btn_config),
         ])
 
         # Layout (Row direction)
         main_layout = QHBoxLayout()
-        main_layout.addWidget(sidebar_widget)
-        main_layout.addWidget(stacked_widget, stretch=1)  # The router takes up remaining space
+        main_layout.addWidget(sidebar)
+        main_layout.addWidget(stacked_router, stretch=1)  # The router takes up remaining space
         main_layout.setSpacing(0)
         main_layout.setContentsMargins(0, 0, 0, 0)
 
--- src/file_conversor/gui/_widgets/sidebar_frame.py
+++ src/file_conversor/gui/_widgets/sidebar_frame.py
@@ -3,8 +3,9 @@
 from pathlib import Path
 from typing import override
 
-from PySide6.QtWidgets import QFrame, QVBoxLayout
+from PySide6.QtWidgets import QFrame, QLayout, QVBoxLayout, QWidget
 
+from file_conversor.gui._utils import Stretch
 from file_conversor.gui._widgets.scrollarea import ScrollArea
 
 
@@ -28,6 +29,27 @@ class SidebarFrame(ScrollArea):
         frame.setLayout(self._layout)
 
         self.setWidget(frame)
+
+    def addWidget(self, widget: QWidget) -> None:
+        self._layout.addWidget(widget)
+
+    def addLayout(self, layout: QLayout) -> None:
+        self._layout.addLayout(layout)
+
+    def addStretch(self, stretch: int = 1) -> None:
+        self._layout.addStretch(stretch)
+
+    def addItems(self, *items: QWidget | QLayout | Stretch) -> None:
+        for item in items:
+            if isinstance(item, QWidget):
+                self.addWidget(item)
+            elif isinstance(item, Stretch):
+                self.addStretch(item.stretch)
+            else:
+                self.addLayout(item)
+
+    def getStretch(self, stretch: int = 1) -> Stretch:
+        return Stretch(stretch)
 
     @override
     def layout(self) -> QVBoxLayout:
